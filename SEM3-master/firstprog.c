7a449b086a22a340c277c3bedac583459a732a1f
Ljetulf <vezon-x@mail.ru>
2017-10-19 22:07:26 +0300

    Fixed

--- SEM3-master/firstprog.c
+++ SEM3-master/firstprog.c
@@ -3,23 +3,13 @@
 
 void GenerateString(int n, char* string)
 {
-    if (!(string=(char *) malloc(sizeof(char)*(1 << n))))
-    // fixit: пробелы вокруг бинарных операторов (здесь и ниже)
-        printf("Error in memory allocate\n");
-    else
+    string[0] = 'a';
+    for (int i = 1; i < n; i++)
     {
-        string[0]='a';
-        for (int i=1;i<n;i++)
-        {
-            strncpy(string+(1<<i),string,(1<<i)-1);
-            // вам точно string[(1 << i) - 1] = 'a' + i; меньше нравится?
-            *(string + (1 << i) - 1)='a'+i;
-        }
-        *(string + (1 << n) - 1)='\0';
+        string[(1 << i) - 1] = 'a' + i;
+	*(string + (1 << i) - 1) = 'a' + i;
     }
-    printf(string);
-    // fixit: выделение/освобождение памяти + вывод вынесите из ф-и наружу
-    free((void *)string);
+    *(string + (1 << n) - 1) = '\0';
 }
 
 int main()
@@ -27,6 +17,11 @@ int main()
     int a;
     char *s;
     scanf("%d",&a);
-    GenerateString(a,s);
+    if (!(string = (char *) malloc(sizeof(char) * (1 << n))))
+        printf("Error in memory allocate\n");
+    else    
+    	GenerateString(a,s);
+    printf(s);
+    free(s);
     return 0;
 }
